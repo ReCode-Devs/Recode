aae62c8191e754d47dfa04b87712aa3f399b08cb
Ad7am-14 <131503352+Ad7am-14@users.noreply.github.com>
2023-06-10 15:03:56 +0100

    .

--- js/script.js
+++ js/script.js
@@ -24,6 +24,34 @@ window.addEventListener("scroll", function () {
   }
 });
 
+const header = document.querySelector("header .container");
+let prevScrollpos = window.scrollY;
+let currentScrollPos;
+let timeout;
+
+window.onscroll = function () {
+  currentScrollPos = window.scrollY;
+
+  if (prevScrollpos > currentScrollPos) {
+    // Scrolling up, show the header
+    header.style.top = 25 + "px";
+    header.style.transition = "top 0.5s ease-in-out";
+  } else {
+    // Scrolling down, hide the header
+    header.style.top = "-100px";
+    header.style.transition = "top 0.5s ease-in-out";
+  }
+
+  prevScrollpos = currentScrollPos;
+
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    // Delayed animation, show the header
+    header.style.top = 25 + "px";
+    header.style.transition = "top 0.5s ease-in-out";
+  }, 500);
+};
+
 // Scroll to about-us section on button click
 const pageDownBtn = document.getElementById("page-down");
 pageDownBtn.addEventListener("click", function () {
